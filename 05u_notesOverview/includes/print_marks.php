776ac1453fe9a6d7ced277633ed55196c469f279
acidyumyum <acidyumyum@web.de>
2017-12-06 23:07:41 +0100

    added stuff of notes overview exercise and formulars

---
+++ 05u_notesOverview/includes/print_marks.php
@@ -0,0 +1,14 @@
+<form action="" method="post">
+
+<table border="1">
+<tbody>
+    <tr>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Mark</th>
+        <th>Attempt</th>
+    </tr>
+</tbody>
+</table>
+<input type="submit" name="deleteForm" value="Auswahl löschen" class="del-btn">
+</form>
